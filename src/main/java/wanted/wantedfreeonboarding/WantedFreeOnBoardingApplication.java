383da50e7f2dc143c39831652269916b8e8d1be6
진현규 <108057548+jinhyeonkyu@users.noreply.github.com>
2024-01-21 18:07:54 +0900

    Todo : 프리온보딩 백엔드 1월 추가 과제

---
+++ src/main/java/wanted/wantedfreeonboarding/WantedFreeOnBoardingApplication.java
@@ -0,0 +1,13 @@
+package wanted.wantedfreeonboarding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WantedFreeOnBoardingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WantedFreeOnBoardingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wanted/wantedfreeonboarding/WantedFreeOnBoardingApplicationTests.java
@@ -0,0 +1,13 @@
+package wanted.wantedfreeonboarding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WantedFreeOnBoardingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
